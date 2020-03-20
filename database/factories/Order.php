bb05780a9a5589dd2784fb6d6ddd206db330e05f
Khorn Din <kh.din99@gmail.com>
2020-03-20 15:47:12 +0700

    on change order

--- database/factories/Order.php
+++ database/factories/Order.php
@@ -7,12 +7,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Order::class, function (Faker $faker) {
     return [
-        'member_id' => \App\Member::all()->random()->id,
+        // 'member_id' => \App\Member::all()->random()->id,
         // 'name' => $faker->name,
         // 'code' => $faker->randomNumber($nbDigits = NULL),
-        'description' => $faker->text,
-        'active' => $faker->randomElement(['1', '0']),
-        'discount' => $faker->randomElement(['0.1', '0.2', '0.5']), 
+        // // 'description' => $faker->text,
+        // 'active' => $faker->randomElement(['1', '0']),
+        // 'discount' => $faker->randomElement(['0.1', '0.2', '0.5']), 
         // 'tax' => $faker->randomFloat(2, 0, 200), 
         // 'sub_total' => $faker->randomFloat(2, 1000, 200000),
     ];
